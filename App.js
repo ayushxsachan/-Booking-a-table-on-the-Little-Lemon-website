9479f0a70e93a727cce18b1f4666cbb40551f101
Ayush Sachan <166961613+ayushxsachan@users.noreply.github.com>
2024-07-25 02:17:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookingForm from './BookingForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Little Lemon</h1>
+      </header>
+      <BookingForm />
+    </div>
+  );
+};
+
+export default App;
